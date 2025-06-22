e30c71089e5b40fd55e56a126669703ef69d8d20
DobJalo <marugrig@gmail.com>
2025-06-22 14:02:16 +0300

    Second commit

---
+++ Assets/Scripts/MoneyManager.cs
@@ -0,0 +1,89 @@
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[System.Serializable]
+public class SaveMoney
+{
+    public int money;
+}
+
+public static class SaveLoad
+{
+    public static void Save(int moneyAmount, string fileName)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Path.Combine(Application.persistentDataPath, fileName);
+        using (FileStream file = File.Create(path))
+        {
+            SaveMoney data = new SaveMoney();
+            data.money = moneyAmount;
+
+            formatter.Serialize(file, data);
+        }
+    }
+
+    public static SaveMoney Load(string fileName)
+    {
+        string path = Path.Combine(Application.persistentDataPath, fileName);
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream file = File.Open(path, FileMode.Open))
+            {
+                return formatter.Deserialize(file) as SaveMoney;
+            }
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public static void ResetData(string fileName)
+    {
+        string path = Path.Combine(Application.persistentDataPath, fileName);
+        if (File.Exists(path))
+        {
+            File.Delete(path);
+        }
+    }
+}
+
+public class MoneyManager : MonoBehaviour
+{
+    private string fileName = "MoneyAmount.dat";
+    public Text moneyText;
+    private int currentMoney;
+
+    private void Start()
+    {
+        SaveMoney data = SaveLoad.Load(fileName);
+        if (data == null)
+        {
+            currentMoney = 10;
+        }
+        else
+        {
+            currentMoney = data.money;
+        }
+
+        moneyText.text = currentMoney.ToString();
+
+        // Пример сохранения
+        // SaveLoad.Save(currentMoney, fileName);
+    }
+
+
+
+
+    void ResetData(string fileName)
+    {
+        if (File.Exists(Application.persistentDataPath + fileName))
+        {
+            File.Delete(Application.persistentDataPath + fileName);
+        }
+    }
+}
---
+++ Assets/Scripts/SaveScreenshot.cs
@@ -0,0 +1,106 @@
+using UnityEngine;
+using System;
+using System.Collections;
+using System.IO;
+using System.Text.RegularExpressions;
+
+public class SaveScreenshot : MonoBehaviour
+{
+    public GameObject infoMessage;
+    string folderPath;
+    string fileName;
+    Texture2D tex;
+    byte[] bytes;
+
+
+    public void TakeScreenshot()
+    {
+        StartCoroutine(TakeScreenShotPC());
+        //StartCoroutine(TakeScreenShotAndroid());
+    }
+
+    /*IEnumerator TakeScreenShotPC() // full screen
+    {
+        yield return new WaitForEndOfFrame();
+
+        string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+        folderPath += "\\";
+
+        ScreenCapture.CaptureScreenshot(System.IO.Path.Combine(folderPath, "Magical Fairies_" + System.DateTime.Now.ToString("MM-dd-yy (HH-mm-ss)") + ".png"), 10);
+    }*/
+
+    private void CreateTexture()
+    {
+        tex = new Texture2D(Screen.width / 2, Screen.height, TextureFormat.RGB24, false); // width, height
+        tex.ReadPixels(new Rect(0, 0, Screen.width / 2, Screen.height), 0, 0); // x, y, width, height
+        tex.Apply();
+
+        bytes = tex.EncodeToPNG();
+        UnityEngine.Object.Destroy(tex);
+
+        fileName = "Magical Fairies_" + DateTime.Now.ToString("MM-dd-yy (HH-mm-ss)") + ".png";
+    }
+
+    private System.Collections.IEnumerator TakeScreenShotPC()
+    {
+        yield return new WaitForEndOfFrame();
+
+        CreateTexture();
+
+        string folder = Path.Combine(Application.persistentDataPath, "Screenshots");
+
+        // Create folder
+        if (!Directory.Exists(folder))
+            Directory.CreateDirectory(folder);
+
+        folderPath = Path.Combine(folder, fileName);
+
+        // save
+        File.WriteAllBytes(folderPath, bytes);
+
+#if UNITY_EDITOR || UNITY_STANDALONE_WIN
+        // show folder
+        Application.OpenURL("file://" + folder);
+#endif
+    }
+
+
+    private IEnumerator TakeScreenShotAndroid()
+    {
+        yield return new WaitForEndOfFrame();
+
+        CreateTexture();
+
+        folderPath = "/storage/emulated/0/Pictures/MagicalFairies";
+
+        if (!Directory.Exists(folderPath))
+            Directory.CreateDirectory(folderPath);
+
+        string fullPath = Path.Combine(folderPath, fileName);
+        File.WriteAllBytes(fullPath, bytes);
+
+#if UNITY_ANDROID && !UNITY_EDITOR
+    // Обновляем галерею
+    using (AndroidJavaClass mediaScanner = new AndroidJavaClass("android.media.MediaScannerConnection"))
+    using (AndroidJavaObject activity = new AndroidJavaClass("com.unity3d.player.UnityPlayer")
+                .GetStatic<AndroidJavaObject>("currentActivity"))
+    {
+        mediaScanner.CallStatic(
+            "scanFile",
+            activity,
+            new string[] { fullPath },
+            null,
+            null
+        );
+    }
+#endif
+    }
+
+
+}
+
+
+
+
+
+

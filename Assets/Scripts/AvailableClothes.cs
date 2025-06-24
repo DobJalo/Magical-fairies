dfbe4ae2afc81dd07c304502f8203f1384d1c859
DobJalo <marugrig@gmail.com>
2025-06-24 14:46:05 +0300

    Clothes Arrays

---
+++ Assets/Scripts/AvailableClothes.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvailableClothes : MonoBehaviour
+{
+    public GameObject lipsParent;  
+
+    void Start()
+    {
+        int LipsChildCount = lipsParent.transform.childCount;
+
+        for (int i = 0; i < LipsArray.items.Length && i < LipsChildCount; i++)
+        {
+            if (LipsArray.GetValue(i) == 0)
+            {
+                lipsParent.transform.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ClothesMatrix.cs
@@ -0,0 +1,127 @@
+using UnityEngine;
+
+
+public static class LipsArray
+{
+    public static int[] items = new int[13];
+
+    public static int GetValue(int index)
+    {
+        return items[index];
+    }
+
+    public static void SetValue(int index, int value)
+    {
+        items[index] = value;
+    }
+}
+
+public static class EyesArray
+{
+    public static int[] items = new int[24];
+
+    public static int GetValue(int index)
+    {
+        return items[index];
+    }
+
+    public static void SetValue(int index, int value)
+    {
+        items[index] = value;
+    }
+}
+
+public static class EyebrowsArray
+{
+    public static int[] items = new int[9];
+
+    public static int GetValue(int index)
+    {
+        return items[index];
+    }
+
+    public static void SetValue(int index, int value)
+    {
+        items[index] = value;
+    }
+}
+
+public static class HairArray
+{
+    public static int[] items = new int[34];
+
+    public static int GetValue(int index)
+    {
+        return items[index];
+    }
+
+    public static void SetValue(int index, int value)
+    {
+        items[index] = value;
+    }
+}
+
+public static class ClothesTopArray
+{
+    public static int[] items = new int[58];
+
+    public static int GetValue(int index)
+    {
+        return items[index];
+    }
+
+    public static void SetValue(int index, int value)
+    {
+        items[index] = value;
+    }
+}
+
+public static class ClothesBottomArray
+{
+    public static int[] items = new int[50];
+
+    public static int GetValue(int index)
+    {
+        return items[index];
+    }
+
+    public static void SetValue(int index, int value)
+    {
+        items[index] = value;
+    }
+}
+
+public static class HairAccArray
+{
+    public static int[] items = new int[24];
+
+    public static int GetValue(int index)
+    {
+        return items[index];
+    }
+
+    public static void SetValue(int index, int value)
+    {
+        items[index] = value;
+    }
+}
+
+public static class WingsArray
+{
+    public static int[] items = new int[26];
+
+    public static int GetValue(int index)
+    {
+        return items[index];
+    }
+
+    public static void SetValue(int index, int value)
+    {
+        items[index] = value;
+    }
+}
+
+public class ClothesMatrix : MonoBehaviour
+{
+    
+}
